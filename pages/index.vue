9882067ae87f953050075bb47d89e1eff37394bf
Kristjan Jansen <kristjan.jansen@gmail.com>
2022-05-27 01:10:49 +0300

    deploy

--- pages/index.vue
+++ pages/index.vue
@@ -1,4 +1,8 @@
 <script setup lang="ts">
+import { useNow } from "@vueuse/core";
+
+const now = useNow();
+
 const { find } = useStrapi3(); //
 
 // // data, pending, refresh, error
@@ -12,11 +16,8 @@ const { data: events } = await useAsyncData("events", () =>
 </script>
 <template>
   <Stack class="p-8">
-    <Title>Hello world</Title>
-    <div class="flex gap-2">
-      <Button>Hellog</Button>
-      <Button primary>Hellog</Button>
-    </div>
+    <Title>elektron</Title>
+    <p>{{ now }}</p>
     <Markdown>Hello *world*</Markdown>
     <Stack>
       <NuxtLink v-for="event in events" :to="'/events/' + event.slug">
