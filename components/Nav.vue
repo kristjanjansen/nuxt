07d5128f104204f647fc7c38e856098df573aac3
Kristjan Jansen <kristjan.jansen@gmail.com>
2022-05-29 17:23:42 +0300

    adding debug

--- components/Nav.vue
+++ components/Nav.vue
@@ -15,12 +15,13 @@ const { changeTheme } = useTheme();
 </script>
 
 <template>
-  <div class="grid grid-cols-1 md:grid-cols-3">
+  <div class="grid grid-cols-1 md:grid-cols-4">
     <NuxtLink v-for="(n, i) in nav" :to="n.to">
       <NavLink :class="[i === 0 ? 'md:border-l' : '']">{{ n.title }}</NavLink>
     </NuxtLink>
     <button class="text-left" @click="changeTheme">
       <NavLink><IconTheme /></NavLink>
     </button>
+    <NavLink><Messages /></NavLink>
   </div>
 </template>
