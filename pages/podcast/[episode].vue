ecbaefa42d7d57cb5904c26c57d0d78e248dda14
Kristjan Jansen <kristjan.jansen@gmail.com>
2022-06-02 16:28:30 +0300

    podcast data

---
+++ pages/podcast/[episode].vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+const route = useRoute();
+const slug = route.params.episode;
+
+const { data: podcast } = usePodcast();
+const episode = computed(() =>
+  podcast.value?.items.find((i) => i.itunes.episode === slug)
+);
+</script>
+
+<template>
+  <Stack class="debug p-12">
+    <pre>{{ episode }}</pre>
+  </Stack>
+</template>
