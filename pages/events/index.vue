e1031f47436b9502993bdf51fd95a671581a7a03
Kristjan Jansen <kristjan.jansen@gmail.com>
2022-06-06 18:57:53 +0300

    hm

--- pages/events/index.vue
+++ pages/events/index.vue
@@ -1,4 +1,5 @@
 <script setup lang="ts">
+import { Strapi4RequestParams } from "@nuxtjs/strapi/dist/runtime/types";
 import { useNow } from "@vueuse/core";
 import { Event } from "~/composables/event";
 
@@ -6,26 +7,31 @@ definePageMeta({
   title: "Some Page",
 });
 
-const { find } = useStrapi3();
-
+const useStrapiFind = (contentType: string, params?: Strapi4RequestParams) => {
+  const { find } = useStrapi4();
+  return useAsyncData(contentType, () =>
+    find(contentType, params).then(({ data }) => {
+      data.map(({ attributes }) => attributes);
+    })
+  );
+};
 // // data, pending, refresh, error
-const { data: events } = await useAsyncData("events", () =>
-  find<Event[]>("events", { _limit: -1 }).then((events) =>
-    events.reverse().slice(0, 10)
-  )
-);
-
+const { data: events } = useStrapiFind("events", {
+  sort: ["createdAt:desc"],
+  populate: "*",
+});
 // const a = await useAsyncData("hello", () => $fetch("/api/hello"));
 </script>
 <template>
   <Stack class="p-8">
-    <Title class="!md:text-6xl !text-4xl">Projects</Title>
-    <Markdown>Hello *world*</Markdown>
+    <Title class="!md:text-6xl !text-4xl">Events</Title>
+    <p />
     <Stack>
       <NuxtLink v-for="event in events" :to="'/events/' + event.slug">
         <Card class="hover:bg-black">
           <Stack>
             <Title class="text-lg">{{ event.title }}</Title>
+            <pre>{{ event }}</pre>
             <Markdown strip :markdown="event?.description_estonian" />
           </Stack>
         </Card>
