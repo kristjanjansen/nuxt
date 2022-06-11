610c745e85fdfaa271e444261473c4dbd3cd9b64
Kristjan Jansen <kristjan.jansen@gmail.com>
2022-06-11 23:05:44 +0300

    cleanup  drag

--- pages/projects/[project_slug]/[event_slug]/live.vue
+++ pages/projects/[project_slug]/[event_slug]/live.vue
@@ -1,14 +1,39 @@
 <script setup lang="ts">
 import { useDraggable } from "@vueuse/core";
-const url =
-  "https://le21.babahhcdn.com/bb1150-le/x_live_1_c1.smil/playlist.m3u8";
-const video = ref();
-const { width, height, status } = useVideostream(video, url);
-const el = ref<HTMLElement | null>(null);
+const route = useRoute();
+const slug = route.params.event_slug;
 
-const { x, y, style } = useDraggable(el, {
-  initialValue: { x: 40, y: 40 },
-});
+const { data: event } = await useEventBySlug(slug);
+
+//const url = "https://le21.babahhcdn.com/bb1150-le/x_live_1_c1.smil/playlist.m3u8"
+const url = "https://sb.err.ee/live/etv.m3u8";
+
+const video = ref();
+const { width, height } = useVideostream(video, url);
+
+const top = ref("video");
+
+const videoPanel = ref<HTMLElement | null>(null);
+
+const { style: videoStyle, isDragging: isVideoDragging } = useDraggable(
+  videoPanel,
+  {
+    initialValue: { x: 40, y: 40 },
+    onStart: () => (top.value = "video"),
+  }
+);
+
+const aboutPanel = ref<HTMLElement | null>(null);
+
+const { style: aboutStyle, isDragging: isAboutDragging } = useDraggable(
+  aboutPanel,
+  {
+    initialValue: { x: 80, y: 80 },
+    onStart: () => (top.value = "about"),
+  }
+);
+
+const { lang } = useLang();
 </script>
 
 <template>
@@ -18,23 +43,36 @@ const { x, y, style } = useDraggable(el, {
       Back to event
     </Link>
     <div
-      :style="style"
-      class="fixed z-10 w-fit touch-none select-none overflow-hidden rounded border border-white"
+      ref="videoPanel"
+      :style="videoStyle"
+      class="fixed z-10 w-fit cursor-grab touch-none select-none overflow-hidden rounded border border-white"
+      :class="[
+        isVideoDragging ? 'z-40 cursor-grabbing' : '',
+        top === 'video' ? 'z-40' : '',
+      ]"
+    >
+      <video ref="video" controls muted autoplay playsinline class="w-[50vw]" />
+    </div>
+    <div
+      :style="aboutStyle"
+      class="fixed z-10 w-fit cursor-grab touch-none select-none overflow-hidden rounded border border-white"
+      :class="[
+        isAboutDragging ? 'z-40 cursor-grabbing' : '',
+        top === 'about' ? 'z-40' : '',
+      ]"
     >
       <div
-        ref="el"
-        class="cursor-move border-b bg-black/10 px-2 py-1 backdrop-blur-lg"
+        ref="aboutPanel"
+        class="cursor-move border-b bg-black/80 px-2 py-1 backdrop-blur-lg"
       >
-        Video
+        <Stack class="h-[30vw] w-[30vw] overflow-y-scroll p-4">
+          <Title>
+            {{ event.titles[lang] }}
+          </Title>
+          <EventDatetime :event="event" />
+          <Content :content="event.descriptions[lang]" />
+        </Stack>
       </div>
-      <video
-        ref="video"
-        muted
-        autoplay
-        controls
-        :width="width / 3"
-        :height="height / 3"
-      />
     </div>
   </Stack>
 </template>
