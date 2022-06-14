afce9e0b618a06431a98dc5661af179f1032ca5b
Kristjan Jansen <kristjan.jansen@gmail.com>
2022-06-14 07:17:06 +0300

    cleanup

--- pages/lab/videos/[video_id].vue
+++ pages/lab/videos/[video_id].vue
@@ -42,19 +42,21 @@ const onClick = () => (currentX.value = elementX.value);
 watch(currentX, () => {
   currentTime.value = xVideoScale(currentX.value);
 });
+
+const zoom = ref(0);
 </script>
 
 <template>
   <Stack class="p-4 md:p-6" v-if="video">
     {{ video.duration }}
     <Link left to="/lab/videos">Videos</Link>
-    <video ref="videoplayer" controls class="aspect-video w-1/2 rounded" />
     <div class="font-mono text-gray-500">
       <p>startDatetime: {{ video.startDatetime }}</p>
       <p>endDatetime: &nbsp;&nbsp;{{ video.endDatetime }}</p>
       <p>uploadDatetme: {{ video.endDatetime }}</p>
       <p>currentTime: {{ currentTime }}</p>
     </div>
+    <video ref="videoplayer" controls class="aspect-video w-1/2 rounded" />
 
     <input
       type="range"
@@ -65,8 +67,35 @@ watch(currentX, () => {
       class="accent-gray-600"
     />
     <svg ref="svg" :width="width" :height="height" @click="onClick">
-      <rect :width="width" :height="height" fill="rgba(255,255,255,0.1)" />
+      <rect
+        :width="width"
+        :height="height"
+        fill="rgb(var(--white))"
+        opacity="0.2"
+      />
+      <line
+        :x1="currentX"
+        y1="0"
+        :x2="currentX"
+        :y2="height"
+        stroke="rgb(var(--white))"
+        opacity="0.1"
+        :stroke-width="zoom"
+      />
       <line :x1="currentX" y1="0" :x2="currentX" :y2="height" stroke="red" />
     </svg>
+    <input
+      type="range"
+      v-model="zoom"
+      :max="width"
+      step="any"
+      class="accent-gray-300"
+      opacity="0.5"
+    />
+    <p>Paste a CSV here:</p>
+    <textarea
+      rows="10"
+      class="w-full whitespace-pre-wrap border-gray-500 bg-black/0 px-2 py-1 font-mono text-white focus:border-gray-500 focus:ring-0"
+    />
   </Stack>
 </template>
