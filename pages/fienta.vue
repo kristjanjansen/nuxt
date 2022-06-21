40e34b30bb35502cb8ac071882a817dbf45f7314
Kristjan Jansen <kristjan.jansen@gmail.com>
2022-06-22 00:15:48 +0300

    cleanup

--- pages/fienta.vue
+++ pages/fienta.vue
@@ -6,25 +6,24 @@ const { lang } = useLang();
 </script>
 
 <template>
-  <Stack class="p-4 md:p-6">
+  <Stack
+    class="grid h-full place-content-center place-items-center p-8 text-center"
+  >
     <Title>{{ ["Validate your ticket", "Piletikontroll"][lang] }}</Title>
     <Content
-      v-if="lang === 0"
+      class="max-w-[40ch]"
       :content="
         parseMarkdown(
-          `There is a **ticket code** in your Fienta email, just below the Sisene üritusele / Enter event blue button. Copy-paste the code to the box below:`
-        )
-      "
-    />
-    <Content
-      v-if="lang === 1"
-      :content="
-        parseMarkdown(
-          `Fienta piletil on piletikood emaili lõpus, sinise sisenemisnupu all. Kopeeri see kood siia:`
+          [
+            `There is a **ticket code** in your Fienta email, just below big blue button. Copy-paste the code into the box below:`,
+            `Fienta piletil on piletikood emaili lõpus, sinise sisenemisnupu all. Kopeeri see kood siia:`,
+          ][lang]
         )
       "
     />
     <Textarea v-model="code" placeholder="Code" class="w-[10ch]"></Textarea>
-    <Button primary>Submit ticket code</Button>
+    <Button primary>{{
+      ["Submit ticket code", "Kontrolli pileti koodi"][lang]
+    }}</Button>
   </Stack>
 </template>
