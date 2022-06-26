028608f8575d38dceabdffc1221c25dd465f710c
Kristjan Jansen <kristjan.jansen@gmail.com>
2022-06-26 19:35:50 +0300

    cleanup

---
+++ components/ProjectCard.vue
@@ -0,0 +1,23 @@
+<script setup lang="ts">
+type Props = {
+  project: any;
+};
+const { project } = defineProps<Props>();
+const { lang } = useLang();
+</script>
+
+<template>
+  <Stack>
+    <Image
+      class="aspect-square h-full rounded-2xl object-cover"
+      :image="project.thumbnail"
+    />
+    <!-- <Title class="block">
+      {{ project.titles[lang] }}
+    </Title>
+    <div v-for="event in project.events">
+      <Title small>{{ event.title }}</Title>
+      <EventDatetime :event="event" />
+    </div> -->
+  </Stack>
+</template>
--- pages/projects/index.vue
+++ pages/projects/index.vue
@@ -4,39 +4,43 @@ definePageMeta({
   title: "Some Page",
 });
 
-const { data: pinnnedProjects, error } = await useProjects({
-  filters: { slug: { $notIn: ["signal"] } },
+const { data: currentProjects, error: currentError } = await useProjects({
+  filters: { slug: { $notIn: ["signal", "kohe2022"] }, pinned: { $eq: true } },
+});
+
+const { data: pastProjects, error: pastError } = await useProjects({
+  filters: { slug: { $notIn: ["signal", "kohe2022"] }, pinned: { $eq: false } },
 });
 const { lang } = useLang();
 </script>
 
 <template>
-  <ErrorCard v-if="error" />
+  <ErrorCard v-if="currentError || pastError" />
   <Stack v-else class="p-4 md:p-6">
     <Link left to="/" />
     <Title>
-      {{ ["Projects", "Projektid"][lang] }}
+      {{ ["Current projects", "Käimasolevad projektid"][lang] }}
     </Title>
-    <div class="flex flex-col gap-6 md:grid md:grid-cols-4 md:gap-4">
+    <div class="flex flex-col gap-6 md:grid md:grid-cols-3 md:gap-4">
       <NuxtLink
-        v-for="project in pinnnedProjects"
+        v-for="project in currentProjects"
         class="block h-full"
         :to="'/projects/' + project.slug"
       >
-        <Stack>
-          <Image
-            class="aspect-square h-full rounded-2xl object-cover"
-            :image="project.thumbnail"
-          />
-          <Title class="block">
-            {{ project.titles[lang] }}
-          </Title>
-          <pre>{{ project.slug }}</pre>
-          <div v-for="event in project.events">
-            <Title small>{{ event.title }}</Title>
-            <EventDatetime :event="event" />
-          </div>
-        </Stack>
+        <ProjectCard :project="project" />
+      </NuxtLink>
+    </div>
+    <div class="h-8" />
+    <Title>
+      {{ ["Past projects", "Varasemad projektid"][lang] }}
+    </Title>
+    <div class="flex flex-col gap-6 md:grid md:grid-cols-3 md:gap-4">
+      <NuxtLink
+        v-for="project in pastProjects"
+        class="block h-full"
+        :to="'/projects/' + project.slug"
+      >
+        <ProjectCard :project="project" />
       </NuxtLink>
     </div>
   </Stack>
