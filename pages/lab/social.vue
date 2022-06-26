8ca7402f277fef6d532bbd14ab66bffb27ad016f
Kristjan Jansen <kristjan.jansen@gmail.com>
2022-06-26 18:43:12 +0300

    feat: social images, fonts and layouts (#27)

---
+++ pages/lab/social.vue
@@ -0,0 +1,15 @@
+<script>
+definePageMeta({
+  layout: false,
+});
+</script>
+
+<template>
+  <div
+    class="grid h-[512px] w-[1024px] place-content-center place-items-center bg-black text-center text-white"
+  >
+    <div class="font-title text-8xl font-medium">
+      eˉlektron<span class="text-[red]">.</span>art
+    </div>
+  </div>
+</template>
