14182eca6a7c00ede5c230e87b1af8a0b3a5a4e5
Kristjan Jansen <kristjan.jansen@gmail.com>
2022-06-27 18:53:58 +0300

    cleanup

--- pages/lab/draggables.vue
+++ pages/lab/draggables.vue
@@ -3,77 +3,94 @@ import { useDraggable } from "@vueuse/core";
 
 const useDraggables = (initialDraggables: any) => {
   const makeDraggable = (initialDraggable: any) => {
-    // const draggable = ref<HTMLElement | null>(null);
-    // const { style, isDragging } = useDraggable(draggable, {
-    //   initialValue: { x: initialDraggable.x, y: initialDraggable.y },
-    //   onStart: () => {},
-    // });
-    const front = ref(0);
-    const onStart = () => {
-      front.value = Math.random();
-    };
-    const d = { ...initialDraggable, front, onStart };
-    return d;
+    const front = ref(false);
+    const docked = ref(false);
+    return { ...initialDraggable, front, docked };
   };
 
-  let draggables = Object.fromEntries(
-    Object.entries(initialDraggables).map(([key, draggable]) => [
-      key,
-      {
-        ...makeDraggable(draggable),
-        onStart: () => {
-          Object.keys(draggables).forEach((k) => (draggables[k].front = false));
-          draggables[key].front = true;
+  let draggables = reactive(
+    Object.fromEntries(
+      Object.entries(initialDraggables).map(([key, draggable]) => [
+        key,
+        {
+          ...makeDraggable(draggable),
+          onStart: () => {
+            Object.keys(draggables).forEach(
+              (k) => (draggables[k].front = false)
+            );
+            draggables[key].front = true;
+          },
+          toggleDock: () => {
+            draggables[key].docked = !draggables[key].docked;
+            draggables[key].onStart();
+          },
         },
-      },
-    ])
+      ])
+    )
   );
 
   return draggables;
 };
-const { first, second } = useDraggables({
+const d = useDraggables({
   first: { x: 300, y: 300 },
   second: { x: 400, y: 400 },
 });
 
 const draggable1 = ref<HTMLElement | null>(null);
 const { style: style1, isDragging: isDragging1 } = useDraggable(draggable1, {
-  initialValue: { x: first.x, y: first.y },
-  onStart: first.onStart,
+  initialValue: { x: d.first.x, y: d.first.y },
+  onStart: d.first.onStart,
 });
 
 const draggable2 = ref<HTMLElement | null>(null);
 const { style: style2, isDragging: isDragging2 } = useDraggable(draggable2, {
-  initialValue: { x: second.x, y: second.y },
-  onStart: second.onStart,
+  initialValue: { x: d.second.x, y: d.second.y },
+  onStart: d.second.onStart,
 });
 </script>
 
 <template>
   <div class="p-4 md:p-6">
-    <pre>{{ first }}</pre>
-    <pre>{{ second }}</pre>
+    <pre>{{ d.first }}</pre>
+    <div class="flex">
+      <div v-for="(dock, key) in d" class="border p-4">
+        {{ key }} / {{ dock.docked }}
+        <Button v-if="dock.docked" @click="d[key].toggleDock">Undock</Button>
+      </div>
+    </div>
     <div
       ref="draggable1"
       class="z-10 w-full cursor-grab touch-none select-none overflow-hidden rounded border border-gray-700 bg-black/80 p-16 backdrop-blur-lg transition-colors md:fixed md:w-fit md:border-gray-500 md:hover:border-gray-400"
       :class="[
         isDragging1 ? 'z-40 cursor-grabbing !border-gray-100' : '',
-        first.front ? 'z-40' : '',
+        d.first.front ? 'z-40' : '',
+        d.first.docked ? 'opacity-10' : '',
       ]"
       :style="style1"
     >
-      Hello {{ first.front }}
+      <Stack>
+        <div>First front {{ d.first.front }}</div>
+        <div>docked {{ d.first.docked }}</div>
+        <Button @click="d.first.toggleDock"> Dock </Button>
+      </Stack>
     </div>
     <div
       ref="draggable2"
       class="z-10 w-full cursor-grab touch-none select-none overflow-hidden rounded border border-gray-700 bg-black/80 p-16 backdrop-blur-lg transition-colors md:fixed md:w-fit md:border-gray-500 md:hover:border-gray-400"
       :class="[
         isDragging2 ? 'z-40 cursor-grabbing !border-gray-100' : '',
-        second.front ? 'z-40' : '',
+        d.second.front ? 'z-40' : '',
+        d.second.docked ? 'opacity-20' : '',
       ]"
       :style="style2"
     >
-      Hello {{ second.front }}
+      <Stack>
+        <div>Second front {{ d.second.front }}</div>
+        <div>dock {{ d.second.docked }}</div>
+        <Button v-if="!d.second.docked" @click="d.second.toggleDock"
+          >Dock</Button
+        >
+      </Stack>
     </div>
   </div>
 </template>
