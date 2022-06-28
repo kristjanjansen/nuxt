8e1b77a8509d02ad1f408a8c757c457976a56767
Kristjan Jansen <kristjan.jansen@gmail.com>
2022-06-28 15:05:28 +0300

    cleanup

--- pages/lab/draggables.vue
+++ pages/lab/draggables.vue
@@ -1,46 +1,7 @@
 <script setup lang="ts">
-const useDraggables = (initialDraggables: any) => {
-  const indexes = ref([]);
-
-  const keys = Object.keys(initialDraggables);
-  const draggables = keys.map((key) => {
-    const x = ref(initialDraggables[key].x);
-    const y = ref(initialDraggables[key].y);
-    const updateIndex = () =>
-      (indexes.value = unique([...indexes.value, key].reverse()).reverse());
-    const updateXY = ({ x: newX, y: newY }) => {
-      x.value = newX;
-      y.value = newY;
-      updateIndex();
-    };
-    const docked = ref(initialDraggables[key].docked || false);
-    const setDocked = () => {
-      docked.value = !docked.value;
-      updateIndex();
-    };
-    const getIndex = () => {
-      const index = indexes.value.findIndex((index) => index === key);
-      return index > -1 ? index + 1 : "";
-    };
-    const getDocked = () => docked.value;
-    return {
-      ...initialDraggables[key],
-      x,
-      y,
-      updateXY,
-      setDocked,
-      getDocked,
-      getIndex,
-    };
-  });
-
-  return Object.fromEntries(keys.map((key, i) => [key, draggables[i]]));
-};
-
 const draggables = useDraggables({
   first: { x: 300, y: 300 },
-  second: { x: 400, y: 400 },
-  third: { x: 500, y: 500 },
+  second: { x: 400, y: 100 },
 });
 </script>
 
