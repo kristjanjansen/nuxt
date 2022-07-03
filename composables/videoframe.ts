713d683a0acb700845710f0903f4e7357af0886f
Kristjan Jansen <kristjan.jansen@gmail.com>
2022-07-03 22:46:14 +0300

    feat: detect empty canvas

--- composables/videoframe.ts
+++ composables/videoframe.ts
@@ -4,6 +4,19 @@ export const useVideoframe = (videoRef, canvasRef, width, height) => {
   const frames = ref([]);
   const frame = ref();
 
+  const isEmptyFrame = () => {
+    if (!context.value && !canvasRef.value) return true;
+    const buffer = new Uint32Array(
+      context.value.getImageData(
+        0,
+        0,
+        canvasRef.value.width,
+        canvasRef.value.width
+      ).data.buffer
+    );
+    return buffer.some((color) => color === 0);
+  };
+
   watch([videoRef, canvasRef, width, height], () => {
     if (videoRef.value && canvasRef.value) {
       context.value = canvasRef.value.getContext("2d");
@@ -24,11 +37,14 @@ export const useVideoframe = (videoRef, canvasRef, width, height) => {
         height.value * canvasMultiplier
       );
     }
-    frame.value = {
-      src: canvasRef.value.toDataURL("image/jpeg", 0.7),
-      timestamp: performance.now(),
-    };
-    frames.value.push(frame.value);
+    if (!isEmptyFrame()) {
+      frame.value = {
+        // @TODO: Simplify the data
+        src: canvasRef.value.toDataURL("image/jpeg", 0.7),
+        timestamp: performance.now(),
+      };
+      frames.value.push(frame.value);
+    }
   };
 
   return { context, capture, frame, frames };
