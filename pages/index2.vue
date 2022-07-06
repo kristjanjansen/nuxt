c64d70a9842d04da612845d0bb49a386a9ac36c5
Kristjan Jansen <kristjan.jansen@gmail.com>
2022-07-07 00:37:31 +0300

    feat: frontpage

--- pages/index2.vue
+++ pages/index2.vue
@@ -16,33 +16,48 @@ const d = useDraggables({
 
 //const url = videostreams[0].url
 const url = "https://sb.err.ee/live/vikerraadio.m3u8";
+
+const wallpapers = [
+  "https://elektron.fra1.cdn.digitaloceanspaces.com/assets/wallpaper2.jpg",
+];
+const { theme } = useTheme();
 </script>
 
 <template>
   <Stack class="p-4">
-    <Breadboard class="bg-white/10" />
+    <img
+      :src="wallpapers[0]"
+      class="fixed inset-0 h-full w-full object-cover"
+    />
+    <div
+      class="fixed inset-0 h-full w-full backdrop-blur md:backdrop-blur-none"
+      :class="['bg-black/60', 'bg-black/80'][theme]"
+    />
     <Stack class="absolute top-4 left-4 right-4 gap-4 md:top-6 md:left-6">
-      <Draggable v-bind="d.project1">
-        <Stack class="grid h-[30vw] w-[70vw] grid-cols-2 overflow-auto p-4">
-          <Stack>
-            <Title>{{ project1.titles[lang] }}</Title>
-            <Content :content="project1.intros[lang]" />
-          </Stack>
-          <EventCard v-for="event in project1.events" :event="event" />
-        </Stack>
-      </Draggable>
       <Draggable v-bind="d.event1">
         <img
           class="pointer-events-none absolute inset-0 h-full w-full touch-none object-cover opacity-40"
           src="https://www.baltoscandal.ee/sites/default/files/inline/images/elektronfamily_alissasnaider_2022-149_1000.jpg"
         />
-        <div class="h-[20vw] w-[40vw] overflow-auto p-4">
+        <div class="h-[60vh] overflow-auto p-4 md:h-[20vw] md:w-[40vw]">
           <Stack>
             <Title>{{ event1.titles[lang] }}</Title>
             <Content :content="event1.intros[lang]" />
           </Stack>
           <Audiostream class="absolute right-0 bottom-6 left-0" :url="url" />
         </div>
+      </Draggable>
+
+      <Draggable v-bind="d.project1">
+        <Stack
+          class="grid overflow-auto p-4 md:h-[30vw] md:w-[70vw] md:grid-cols-2"
+        >
+          <Stack>
+            <Title>{{ project1.titles[lang] }}</Title>
+            <Content :content="project1.intros[lang]" />
+          </Stack>
+          <EventCard v-for="event in project1.events" :event="event" />
+        </Stack>
       </Draggable>
     </Stack>
     <Dock :draggables="d" />
