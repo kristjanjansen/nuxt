bb4e197c11103366ae47382adbd791ea8a3c323b
Kristjan Jansen <kristjan.jansen@gmail.com>
2022-08-29 13:25:16 +0300

    streaming

--- pages/lab/streaming.vue
+++ pages/lab/streaming.vue
@@ -1,9 +1,27 @@
 <script setup lang="ts">
-const url = "https://streaming.elektron.art/hls/stream6.m3u8";
+const url1 = "https://streaming.elektron.art/hls/test1.m3u8";
+const url2 = "https://streaming.elektron.art/hls/test2.m3u8";
 </script>
 
 <template>
-  <Stack class="grid grid-cols-2 p-4 md:p-6">
-    <Videostream :url="url" />
+  <Stack class="grid grid-cols-2 gap-8 p-4 md:p-6">
+    <Stack>
+      <Title>No transcoding</Title>
+      <Title medium>Instructions</Title>
+      <p>Stream URL:</p>
+      <pre>rtmp://streaming.elektron.art/raw</pre>
+      <p>Streamkey</p>
+      <pre>test1</pre>
+      <Videostream :url="url1" />
+    </Stack>
+    <Stack>
+      <Title>With transcoding</Title>
+      <Title medium>Instructions</Title>
+      <p>Stream URL:</p>
+      <pre>rtmp://streaming.elektron.art/transcoding</pre>
+      <p>Streamkey</p>
+      <pre>test2</pre>
+      <Videostream :url="url2" />
+    </Stack>
   </Stack>
 </template>
