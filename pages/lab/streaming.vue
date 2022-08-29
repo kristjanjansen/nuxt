834de5ab9ddc54458b97f25080086936b25f4e74
Kristjan Jansen <kristjan.jansen@gmail.com>
2022-08-29 19:02:09 +0300

    cleanup

--- pages/lab/streaming.vue
+++ pages/lab/streaming.vue
@@ -4,24 +4,24 @@ const url2 = "https://streaming.elektron.art/hls/test2.m3u8";
 </script>
 
 <template>
-  <Stack class="grid grid-cols-2 gap-8 p-4 md:p-6">
-    <Stack>
-      <Title>No transcoding</Title>
-      <Title medium>Instructions</Title>
-      <p>Stream URL:</p>
-      <pre>rtmp://streaming.elektron.art/raw</pre>
-      <p>Streamkey</p>
-      <pre>test1</pre>
-      <Videostream :url="url1" />
-    </Stack>
-    <Stack>
-      <Title>With transcoding</Title>
-      <Title medium>Instructions</Title>
-      <p>Stream URL:</p>
-      <pre>rtmp://streaming.elektron.art/transcoding</pre>
-      <p>Streamkey</p>
-      <pre>test2</pre>
-      <Videostream :url="url2" />
-    </Stack>
-  </Stack>
+  <Stack class="p-4 md:p-6">
+    <Title>Streaming test</Title>
+    <div class="grid grid-cols-2 gap-8">
+      <Stack>
+        <Title medium>No transcoding</Title>
+        <p>Stream URL:</p>
+        <pre>rtmp://streaming.elektron.art/raw</pre>
+        <p>Streamkey</p>
+        <pre>test1</pre>
+        <Videostream :url="url1" class="aspect-video w-full" />
+      </Stack>
+      <Stack>
+        <Title medium>With transcoding</Title>
+        <p>Stream URL:</p>
+        <pre>rtmp://streaming.elektron.art/transcoding</pre>
+        <p>Streamkey</p>
+        <pre>test2</pre>
+        <Videostream :url="url2" class="aspect-video w-full" />
+      </Stack></div
+  ></Stack>
 </template>
