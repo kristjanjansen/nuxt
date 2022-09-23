36eb21e29d8f5b96d4a43989c8b4e7fa2d070515
Kristjan Jansen <kristjan.jansen@gmail.com>
2022-09-23 23:50:44 +0300

    fix

--- composables/files.ts
+++ composables/files.ts
@@ -1,4 +1,4 @@
-import { sub, parse } from "date-fns";
+import { sub, parse, add } from "date-fns";
 
 export const processFile = (file: any, path) => {
   file.pathRoute = `/lab/files/${path}`;
@@ -9,14 +9,14 @@ export const processFile = (file: any, path) => {
   let f = file.filename.replace(/\.mp4$/, "").split("___");
   if (f.length === 3) {
     file.streamkey = f[0];
-    file.end_at_raw = f[1];
-    file.end_at = parse(f[1] + "__Z", "yy_MM_dd__HH_mm_ss__X", new Date());
-    file.endDatetime = file.end_at;
-    file.end_at_formatted = formatDatetime(new Date(file.end_at));
-    file.duration = parseFloat(f[2]);
-    file.start_at = sub(file.end_at, { seconds: file.duration });
-    file.start_at_formatted = formatDatetime(file.start_at);
+    file.start_at_raw = f[1];
+    file.start_at = parse(f[1] + "__Z", "yy_MM_dd__HH_mm_ss__X", new Date());
     file.startDatetime = file.start_at;
+    file.start_at_formatted = formatDatetime(new Date(file.start_at));
+    file.duration = parseFloat(f[2]);
+    file.end_at = add(file.start_at, { seconds: file.duration });
+    file.end_at_formatted = formatDatetime(file.end_at);
+    file.endDatetime = file.end_at;
     file.duration_formatted = new Date(file.duration * 1000)
       .toISOString()
       .split("T")[1]
