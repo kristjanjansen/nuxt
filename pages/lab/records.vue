488d4197c5a8dbe189b70bd582558f6004f59ac2
Kristjan Jansen <kristjan.jansen@gmail.com>
2022-09-23 22:26:07 +0300

    records

---
+++ pages/lab/records.vue
@@ -0,0 +1,35 @@
+<script setup lang="ts">
+const { getFiles } = useFiles();
+const { data: files } = await getFiles("records");
+const formatVideo = (file) => {
+  return [
+    `Stars at   ${file.start_at_formatted} ${
+      useFormattedDistance(new Date(file.start_at)).value
+    }`,
+    `Ends at    ${file.end_at_formatted}`,
+    `Duration   ${file.duration_formatted}`,
+  ].join("\n");
+};
+</script>
+
+<template>
+  <Stack class="p-4 md:p-6">
+    <div class="grid grid-cols-3">
+      <div />
+      <div />
+      <Stack>
+        <Card v-for="file in files">
+          <Stack>
+            <video :src="file.src" controls class="w-full4 aspect-video" />
+            <div
+              class="whitespace-pre-wrap break-all font-mono text-sm text-gray-200"
+            >
+              {{ formatVideo(file) }}
+            </div>
+            <Button>Go to video</Button>
+          </Stack>
+        </Card>
+      </Stack>
+    </div>
+  </Stack>
+</template>
