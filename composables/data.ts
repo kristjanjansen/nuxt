8218a29c67faf98c201187aed34b81c28ca11f8b
Kristjan Jansen <kristjan.jansen@gmail.com>
2022-09-25 14:32:04 +0300

    clean

--- composables/data.ts
+++ composables/data.ts
@@ -1,13 +1,4 @@
 import columnify from "columnify";
-// export const formatData = (data) => {
-//   const maxLength = Math.max(...Object.keys(data).map((k) => k.length));
-//   return Object.entries(data)
-//     .map(
-//       ([key, value]) =>
-//         `${(key + ":").padEnd(maxLength + 1)}  ${[value].flat().join(" ")}`
-//     )
-//     .join("\n");
-// };
 
 export const formatData = (data, showHeaders = false) => {
   return columnify(data, {
