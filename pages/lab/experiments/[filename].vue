3dbe549c58d2e7347039af566d366c55e33df8e8
Kristjan Jansen <kristjan.jansen@gmail.com>
2022-09-25 08:53:38 +0300

    clean

--- pages/lab/experiments/[filename].vue
+++ pages/lab/experiments/[filename].vue
@@ -1,5 +1,9 @@
 <script setup lang="ts">
-import { useElementSize, useMediaControls } from "@vueuse/core";
+import {
+  useElementSize,
+  useMediaControls,
+  useMouseInElement,
+} from "@vueuse/core";
 import { scaleLinear } from "d3";
 
 const route = useRoute();
@@ -11,12 +15,17 @@ const { data: files } = await getFiles(path);
 const file = computed(() => {
   return files.value?.filter((file) => file.filename === filename)[0];
 });
+
 const graph = ref(null);
+const svg = ref(null);
+const video = ref(null);
+
 const { width } = useElementSize(graph);
 const height = ref(50);
 
-const videoplayer = ref(null);
-const { currentTime } = useMediaControls(videoplayer);
+const { elementX: scrubX } = useMouseInElement(svg);
+
+const { currentTime } = useMediaControls(video);
 
 const xVideoScale = computed(() =>
   scaleLinear().domain([0, file.value.duration]).range([0, width.value])
@@ -30,11 +39,23 @@ watch([currentTime, width], () => {
 
 const scrubbing = ref(false);
 
+const onScrub = () => {
+  currentTime.value = xVideoScale.value.invert(scrubX.value);
+};
+
 const onMousedown = () => {
   scrubbing.value = true;
+  onScrub();
 };
-const onMousemove = () => {};
-const onMouseup = () => {};
+const onMousemove = () => {
+  if (scrubbing.value) {
+    currentTime.value = xVideoScale.value.invert(scrubX.value);
+  }
+};
+const onMouseup = () => {
+  onScrub();
+  scrubbing.value = false;
+};
 </script>
 
 <template>
@@ -43,7 +64,7 @@ const onMouseup = () => {};
       <Button small to="/lab/experiments" left>Back</Button>
       <Title>{{ file.streamkey }}</Title>
       <video
-        ref="videoplayer"
+        ref="video"
         :src="file.src"
         class="aspect-video w-96"
         playsinline
@@ -60,8 +81,8 @@ const onMouseup = () => {};
       </Code>
       <div ref="graph" class="w-full">
         <svg
-          class="rounded border bg-gray-900"
           ref="svg"
+          class="rounded border bg-gray-900"
           :width="width"
           :height="height"
           @mousedown="onMousedown"
