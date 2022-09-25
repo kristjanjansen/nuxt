352547cd1587cc4dbc663a8d708bc86cdba44161
Kristjan Jansen <kristjan.jansen@gmail.com>
2022-09-25 08:40:09 +0300

    hm

--- pages/lab/experiments/[filename].vue
+++ pages/lab/experiments/[filename].vue
@@ -1,4 +1,7 @@
 <script setup lang="ts">
+import { useElementSize, useMediaControls } from "@vueuse/core";
+import { scaleLinear } from "d3";
+
 const route = useRoute();
 const { filename } = route.params;
 const { getFiles } = useFiles();
@@ -8,21 +11,72 @@ const { data: files } = await getFiles(path);
 const file = computed(() => {
   return files.value?.filter((file) => file.filename === filename)[0];
 });
+const graph = ref(null);
+const { width } = useElementSize(graph);
+const height = ref(50);
+
+const videoplayer = ref(null);
+const { currentTime } = useMediaControls(videoplayer);
+
+const xVideoScale = computed(() =>
+  scaleLinear().domain([0, file.value.duration]).range([0, width.value])
+);
+
+const currentX = ref(0);
+
+watch([currentTime, width], () => {
+  currentX.value = xVideoScale.value(currentTime.value);
+});
+
+const scrubbing = ref(false);
+
+const onMousedown = () => {
+  scrubbing.value = true;
+};
+const onMousemove = () => {};
+const onMouseup = () => {};
 </script>
 
 <template>
-  <Stack v-if="file" class="p-4 md:p-6">
-    <Button small to="/lab/experiments" left>Back</Button>
-    <Title>{{ file.streamkey }}</Title>
-    <Code>
-      {{
-        formatData({
-          "Start at": file.start_at_formatted,
-          "End at": file.end_at_formatted,
-          Duration: file.duration_formatted,
-        })
-      }}
-    </Code>
-    <Code>{{ file }}</Code>
-  </Stack>
+  <div>
+    <Stack v-if="file" class="p-4 md:p-6">
+      <Button small to="/lab/experiments" left>Back</Button>
+      <Title>{{ file.streamkey }}</Title>
+      <video
+        ref="videoplayer"
+        :src="file.src"
+        class="aspect-video w-96"
+        playsinline
+        controls
+      />
+      <Code>
+        {{
+          formatData({
+            "Start at": file.start_at_formatted,
+            "End at": file.end_at_formatted,
+            Duration: file.duration_formatted,
+          })
+        }}
+      </Code>
+      <div ref="graph" class="w-full">
+        <svg
+          class="rounded border bg-gray-900"
+          ref="svg"
+          :width="width"
+          :height="height"
+          @mousedown="onMousedown"
+          @mousemove="onMousemove"
+          @mouseup="onMouseup"
+        >
+          <line
+            :x1="currentX"
+            y1="0"
+            :x2="currentX"
+            :y2="height"
+            class="stroke-red-500"
+          />
+        </svg>
+      </div>
+    </Stack>
+  </div>
 </template>
