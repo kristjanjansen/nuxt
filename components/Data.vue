3eafde22259438dd3f5262ea1083c375a3ec4dc4
Kristjan Jansen <kristjan.jansen@gmail.com>
2022-09-26 11:23:28 +0300

    gm

--- components/Data.vue
+++ components/Data.vue
@@ -1,22 +1,24 @@
 <script setup lang="ts">
 import { useElementSize } from "@vueuse/core";
-import columnify from "columnify";
 
 type Props = {
-  data: object;
+  data: any;
 };
 const { data } = defineProps<Props>();
 
 const el = ref(null);
 const { width } = useElementSize(el);
 const isWide = computed(() => width.value > 1000);
-
+const classes = "whitespace-pre font-mono text-sm text-gray-400";
 const formattedData = computed(() =>
-  columnify(data, {
-    showHeaders: false,
-    dataTransform: (d) =>
-      `<Code class="whitespace-pre font-mono text-sm text-gray-400">${d}</Code>`,
-  })
+  Object.entries(data)
+    .map(
+      ([key, value]) =>
+        `<div class="${classes} text-gray-600">${key}</div><div class="${classes} ${
+          isWide ? "pr-4" : ""
+        }">${value}</div>`
+    )
+    .join("")
 );
 </script>
 
@@ -24,9 +26,7 @@ const formattedData = computed(() =>
   <div
     ref="el"
     v-html="formattedData"
-    class="grid-cols-[auto_1fr] odd:[&>*]:text-gray-600"
-    :class="[
-      isWide ? 'flex flex-wrap gap-x-2 even:[&>*]:pr-4' : 'grid gap-x-3',
-    ]"
+    class="grid-cols-[auto_1fr]"
+    :class="[isWide ? 'flex flex-wrap gap-x-2' : 'grid gap-x-3']"
   />
 </template>
